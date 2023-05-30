0874ffb7d0b63591c4eb189695ae2ae0b8782c30
Prasanjit chetri <prasanjitchetri08@gmail.com>
2023-05-30 13:15:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import ParentForm from './components/ParentForm';
+
+function App() {
+  const mystyle = {
+    color: "black",
+    backgroundColor: "rgb(173,224,196)",
+    padding: "10px",
+    fontFamily: "Arial",
+    width:'100%',
+  };
+  return (
+  <>
+  <div style={mystyle}>
+
+  <ParentForm/>
+  </div>
+  </>
+  );
+}
+
+export default App;
